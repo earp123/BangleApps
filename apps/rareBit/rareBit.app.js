9af6275f44f53108eff96cdc1e85e7eb3afc9348
Sam Rall <rallsw123@gmail.com>
2025-05-19 14:16:16 -0500

    added rareBit app folder

---
+++ apps/rareBit/rareBit.app.js
@@ -0,0 +1,276 @@
+Graphics.prototype.setFontAnton = function(scale) {
+  // Actual height 69 (68 - 0)
+  g.setFontCustom(atob("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADgAAAAAAAAAAA/gAAAAAAAAAAP/gAAAAAAAAAH//gAAAAAAAAB///gAAAAAAAAf///gAAAAAAAP////gAAAAAAD/////gAAAAAA//////gAAAAAP//////gAAAAH///////gAAAB////////gAAAf////////gAAP/////////gAD//////////AA//////////gAA/////////4AAA////////+AAAA////////gAAAA///////wAAAAA//////8AAAAAA//////AAAAAAA/////gAAAAAAA////4AAAAAAAA///+AAAAAAAAA///gAAAAAAAAA//wAAAAAAAAAA/8AAAAAAAAAAA/AAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//////AAAAAB///////8AAAAH////////AAAAf////////wAAA/////////4AAB/////////8AAD/////////+AAH//////////AAP//////////gAP//////////gAP//////////gAf//////////wAf//////////wAf//////////wAf//////////wA//8AAAAAB//4A//wAAAAAAf/4A//gAAAAAAP/4A//gAAAAAAP/4A//gAAAAAAP/4A//wAAAAAAf/4A///////////4Af//////////wAf//////////wAf//////////wAf//////////wAP//////////gAP//////////gAH//////////AAH//////////AAD/////////+AAB/////////8AAA/////////4AAAP////////gAAAD///////+AAAAAf//////4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP/gAAAAAAAAAAP/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/AAAAAAAAAAA//AAAAAAAAAAA/+AAAAAAAAAAB/8AAAAAAAAAAD//////////gAH//////////gAP//////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH/4AAAAB/gAAD//4AAAAf/gAAP//4AAAB//gAA///4AAAH//gAB///4AAAf//gAD///4AAA///gAH///4AAD///gAP///4AAH///gAP///4AAP///gAf///4AAf///gAf///4AB////gAf///4AD////gA////4AH////gA////4Af////gA////4A/////gA//wAAB/////gA//gAAH/////gA//gAAP/////gA//gAA///8//gA//gAD///w//gA//wA////g//gA////////A//gA///////8A//gA///////4A//gAf//////wA//gAf//////gA//gAf/////+AA//gAP/////8AA//gAP/////4AA//gAH/////gAA//gAD/////AAA//gAB////8AAA//gAA////wAAA//gAAP///AAAA//gAAD//8AAAA//gAAAP+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB/+AAAAAD/wAAB//8AAAAP/wAAB///AAAA//wAAB///wAAB//wAAB///4AAD//wAAB///8AAH//wAAB///+AAP//wAAB///+AAP//wAAB////AAf//wAAB////AAf//wAAB////gAf//wAAB////gA///wAAB////gA///wAAB////gA///w//AAf//wA//4A//AAA//wA//gA//AAAf/wA//gB//gAAf/wA//gB//gAAf/wA//gD//wAA//wA//wH//8AB//wA///////////gA///////////gA///////////gA///////////gAf//////////AAf//////////AAP//////////AAP/////////+AAH/////////8AAH///+/////4AAD///+f////wAAA///8P////gAAAf//4H///+AAAAH//gB///wAAAAAP4AAH/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/wAAAAAAAAAA//wAAAAAAAAAP//wAAAAAAAAB///wAAAAAAAAf///wAAAAAAAH////wAAAAAAA/////wAAAAAAP/////wAAAAAB//////wAAAAAf//////wAAAAH///////wAAAA////////wAAAP////////wAAA///////H/wAAA//////wH/wAAA/////8AH/wAAA/////AAH/wAAA////gAAH/wAAA///4AAAH/wAAA//+AAAAH/wAAA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gAAAAAAAAH/4AAAAAAAAAAH/wAAAAAAAAAAH/wAAAAAAAAAAH/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB//8AAA/////+B///AAA/////+B///wAA/////+B///4AA/////+B///8AA/////+B///8AA/////+B///+AA/////+B////AA/////+B////AA/////+B////AA/////+B////gA/////+B////gA/////+B////gA/////+A////gA//gP/gAAB//wA//gf/AAAA//wA//gf/AAAAf/wA//g//AAAAf/wA//g//AAAA//wA//g//gAAA//wA//g//+AAP//wA//g////////gA//g////////gA//g////////gA//g////////gA//g////////AA//gf///////AA//gf//////+AA//gP//////+AA//gH//////8AA//gD//////4AA//gB//////wAA//gA//////AAAAAAAH////8AAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//////gAAAAB///////+AAAAH////////gAAAf////////4AAB/////////8AAD/////////+AAH//////////AAH//////////gAP//////////gAP//////////gAf//////////wAf//////////wAf//////////wAf//////////wAf//////////4A//wAD/4AAf/4A//gAH/wAAP/4A//gAH/wAAP/4A//gAP/wAAP/4A//gAP/4AAf/4A//wAP/+AD//4A///wP//////4Af//4P//////wAf//4P//////wAf//4P//////wAf//4P//////wAP//4P//////gAP//4H//////gAH//4H//////AAH//4D/////+AAD//4D/////8AAB//4B/////4AAA//4A/////wAAAP/4AP////AAAAB/4AD///4AAAAAAAAAH/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//AAAAAAAAAAA//gAAAAAAAAAA//gAAAAAAAAAA//gAAAAAAADgA//gAAAAAAP/gA//gAAAAAH//gA//gAAAAB///gA//gAAAAP///gA//gAAAD////gA//gAAAf////gA//gAAB/////gA//gAAP/////gA//gAB//////gA//gAH//////gA//gA///////gA//gD///////gA//gf///////gA//h////////gA//n////////gA//////////gAA/////////AAAA////////wAAAA///////4AAAAA///////AAAAAA//////4AAAAAA//////AAAAAAA/////4AAAAAAA/////AAAAAAAA////8AAAAAAAA////gAAAAAAAA///+AAAAAAAAA///4AAAAAAAAA///AAAAAAAAAA//4AAAAAAAAAA/+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//gB///wAAAAP//4H///+AAAA///8P////gAAB///+f////4AAD///+/////8AAH/////////+AAH//////////AAP//////////gAP//////////gAf//////////gAf//////////wAf//////////wAf//////////wA///////////wA//4D//wAB//4A//wB//gAA//4A//gA//gAAf/4A//gA//AAAf/4A//gA//gAAf/4A//wB//gAA//4A///P//8AH//4Af//////////wAf//////////wAf//////////wAf//////////wAf//////////gAP//////////gAP//////////AAH//////////AAD/////////+AAD///+/////8AAB///8f////wAAAf//4P////AAAAH//wD///8AAAAA/+AAf//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH//gAAAAAAAAB///+AA/+AAAAP////gA//wAAAf////wA//4AAB/////4A//8AAD/////8A//+AAD/////+A///AAH/////+A///AAP//////A///gAP//////A///gAf//////A///wAf//////A///wAf//////A///wAf//////A///wA///////AB//4A//4AD//AAP/4A//gAB//AAP/4A//gAA//AAP/4A//gAA/+AAP/4A//gAB/8AAP/4A//wAB/8AAf/4Af//////////wAf//////////wAf//////////wAf//////////wAf//////////wAP//////////gAP//////////gAH//////////AAH/////////+AAD/////////8AAB/////////4AAAf////////wAAAP////////AAAAB///////4AAAAAD/////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/AAB/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="), 46, atob("EiAnGicnJycnJycnEw=="), 78 + (scale << 8) + (1 << 16));
+};
+
+const pag_svc = "23210001-28d5-4b7b-bad0-7dee1eee1b6d";
+const pag_chrc = "23210002-28d5-4b7b-bad0-7dee1eee1b6d";
+let setMinutes = 45;
+let remaining = setMinutes * 60;
+let elapsed = 0;
+let timer = null;
+let isRunning = false;
+let finished = false;
+var AR1chrc, AR2chrc;
+let AR1 = {}, AR2 = {};
+let AR1gatt = {
+  connected: false,
+  handle: undefined
+};
+
+let AR2gatt = {
+  connected: false,
+  handle:undefined
+};
+
+
+
+function formatTime(secs) {
+  let mins = Math.floor(secs / 60);
+  let sec = secs % 60;
+  return `${("0" + mins).slice(-2)}:${("0" + sec).slice(-2)}`;
+}
+
+function draw() {
+  g.clear();
+
+  // Count Down
+  g.setFontAnton(1);
+  g.setFontAlign(0, 0);
+  g.setColor("#000");
+  g.drawString(formatTime(remaining), g.getWidth() / 2, g.getHeight() / 2);
+
+  // Count Up
+  g.setFont("6x8", 4);
+  g.setFontAlign(0, 0);
+  g.setColor("#000");
+  g.drawString(formatTime(elapsed), g.getWidth() / 2, g.getHeight() / 2 + 60);
+
+  if (AR1gatt.connected) drawAR1();
+  if (AR2gatt.connected) drawAR2();
+
+}
+
+function tick() {
+  if (remaining > 0) {
+    remaining--;
+    elapsed++;
+    draw();
+  }
+  else if (remaining == 0) {
+    clearInterval(timer);
+    isRunning = false;
+    finished = true;
+    Bangle.buzz();
+    }
+}
+
+function touchHandle()
+{
+  print("Screen Tap");
+  if(!isRunning){
+    setMinutes+=5;
+    if(setMinutes > 45) setMinutes = 10;
+    remaining = setMinutes * 60;
+    elapsed = 0;
+    draw();
+  }
+}
+
+function buttonHandle()
+{
+  print("Button Pressed");
+  if(isRunning){
+    clearInterval(timer);
+    isRunning = false;
+  }
+  else {
+    timer = setInterval(tick, 1000);
+    isRunning = true;
+  }
+}
+
+function drawAR1()
+{
+  // Draw "AR1" in top-left
+  g.setFont("6x8", 3); // Small font, doubled size
+  g.setFontAlign(-1, -1); // Align top-left
+  g.drawString("AR1", 5, 5);
+}
+
+function flashAR1(state) {
+  const x = 5, y = 5;
+  const text = "AR1";
+  const scale = 3;
+
+  g.setFont("6x8", scale);
+  g.setFontAlign(-1, -1);
+
+  const w = g.stringWidth(text);
+  const h = 8 * scale;
+
+  // Background
+  g.setColor(state ? "#000" : "#fff");
+  g.fillRect(x, y, x + w, y + h);
+
+  // Text
+  g.setColor(state ? "#fff" : "#000");
+  g.drawString(text, x, y);
+}
+
+function flashAR2(state) {
+  const text = "AR2";
+  const scale = 3;
+
+  g.setFont("6x8", scale);
+  g.setFontAlign(1, -1); // Right-aligned, top
+  const w = g.stringWidth(text);
+  const h = 8 * scale;
+  const x = g.getWidth() - 5;
+  const y = 5;
+
+  // Background
+  g.setColor(state ? "#000" : "#fff");
+  g.fillRect(x - w, y, x, y + h);
+
+  // Text
+  g.setColor(state ? "#fff" : "#000");
+  g.drawString(text, x, y);
+}
+
+function startFlashingAR(AR) {
+  let state = false;
+  let count = 0;
+  const maxFlashes = 3000 / 200; // 15 flashes (every 200ms for 3s)
+
+  const interval = setInterval(() => {
+    if      (AR == 1) flashAR1(state);
+    else if (AR == 2) flashAR2(state);
+    state = !state;
+    count++;
+    if (count >= maxFlashes) {
+      clearInterval(interval);
+    }
+  }, 200);
+}
+
+function drawAR2()
+{
+  // Draw "AR2" in top-right
+  g.setFont("6x8", 3); // Small font, doubled size
+  g.setFontAlign(1, -1); // Align top-right
+  g.drawString("AR2", g.getWidth() - 5, 5);
+}
+
+function initScan()
+{
+  getDevice();
+
+  let scan = setInterval(function(){
+    if (radioLock){
+      getDevice();
+      clearInterval(scan);
+    }
+  }, 200);
+}
+
+function getDevice(){
+    radioLock = false; //print(radioLock);
+    NRF.requestDevice({phy:"coded", filters:[{ namePrefix:'rareBit'}]})
+      .then(function(device) {
+      if (AR1gatt.handle == undefined) {
+        AR1 = device;
+        AR1gatt = device.gatt;
+        AR1.on('gattserverdisconnected', function(reason) {
+          console.log(device.id, " Disconnected ",reason);
+          let discon_cb = setInterval(function(){
+            if (AR1gatt.handle == undefined){
+              getDevice();
+            }
+            else clearInterval(discon_cb);
+          }, 4000);
+          draw();
+          });
+        return AR1gatt.connect({phy:"coded", minInterval:15, maxInterval:30});
+        }
+      else if (AR2gatt.handle == undefined) {
+        AR2 = device;
+        AR2gatt = device.gatt;
+        AR2.on('gattserverdisconnected', function(reason) {
+          console.log(device.id, " Disconnected ",reason);
+          let discon_cb = setInterval(function(){
+            if (AR2gatt.handle == undefined){
+              getDevice();
+            }
+            else clearInterval(discon_cb);
+          }, 4000);
+          draw();
+          });
+        return AR2gatt.connect({phy:"coded", minInterval:15, maxInterval:30});
+        }
+    }).then(function(gatt){
+      if(gatt == AR1gatt){
+        AR1gatt.getPrimaryService(pag_svc).then(function(s){
+        return s.getCharacteristic(pag_chrc).then(function(c){
+          AR1chrc = c;
+          AR1chrc.on('characteristicvaluechanged', function(event) {
+            console.log("-> ",event.target.service.device.id);
+            startFlashingAR(1);
+            Bangle.buzz(3000, 1);
+          });
+          return AR1chrc.startNotifications();
+        }).then(function(d) {
+            console.log("Waiting for notifications");
+            radioLock = true;
+            drawAR1();
+          }).catch(function() {
+            console.log("Something's broken.");
+            radioLock = true;
+          });
+        });
+      }
+      else if (gatt == AR2gatt){
+        AR2gatt.getPrimaryService(pag_svc).then(function(s){
+        return s.getCharacteristic(pag_chrc).then(function(k){
+          AR2chrc = k;
+          AR2chrc.on('characteristicvaluechanged', function(event) {
+            console.log("-> ", event.target.service.device.id);
+            startFlashingAR(2);
+            Bangle.buzz(3000, 1);
+          });
+          return AR2chrc.startNotifications();
+        }).then(function(d) {
+          console.log("Waiting for notifications");
+          radioLock = true;
+          drawAR2();
+        }).catch(function() {
+          console.log("Something's broken.");
+          radioLock = true;
+        });
+        });
+      }
+    }).catch(function(f){print("No devices found");});
+}
+
+function initUI()
+{
+  g.clear();
+
+  Bangle.setUI({
+      mode:"custom",
+      btn: function() {
+        buttonHandle();
+      },
+      touch: function(zone, e) {
+        touchHandle();
+      }
+  });
+  draw();
+}
+
+// --- Initialize ---
+initUI();
+//Bangle.setLCDPower(1);
+Bangle.setLCDTimeout(0);
+initScan();
+//load("rareBit.app.js");
